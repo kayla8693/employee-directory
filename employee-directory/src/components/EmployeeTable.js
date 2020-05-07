d6bf7765bb6562f39ba925f7c7f7e0c7c56c586e
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-07 04:19:37 -0500

    added name, phone, email, dob to table

--- employee-directory/src/components/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable.js
@@ -4,18 +4,24 @@ function EmployeeTable(props) {
     return (
         <table>
             <thead>
-                <th>Picture</th>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Email</th>
-                <th>DOB</th>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>DOB</th>
+                </tr>
             </thead>
             <tbody>
                 {props.employees.map(employee => (
-                    <tr className="tableRow" key={employee.id.value}>
+                    <tr className="tableRow" key={employee.login.uuid}>
                         <td>
                             <img alt={employee.name.first + " " + employee.name.last} className="employeePic" src={employee.picture.medium} />
                         </td>
+                        <td>{employee.name.first + " " + employee.name.last}</td>
+                        <td>{employee.phone}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.dob.date}</td>
                     </tr>
                 ))}
             </tbody>
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     getUsers: function () {
-        return axios.get('https://randomuser.me/api/?results=100');
+        return axios.get('https://randomuser.me/api/?results=100&nat=us');
         
     }
 };
