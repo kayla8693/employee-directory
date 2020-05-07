a4f6663512b474b20bf277c70fe6aee4848330f4
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-07 03:58:13 -0500

    added employee table, pics

--- employee-directory/src/components/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 function EmployeeTable(props) {
-    return <div value={props.employees} id="result" className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}</div>;
+    return (
+        <table>
+            <thead>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>DOB</th>
+            </thead>
+            <tbody>
+                {props.employees.map(employee => (
+                    <tr className="tableRow" key={employee.id.value}>
+                        <td>
+                            <img alt={employee.name.first + " " + employee.name.last} className="employeePic" src={employee.picture.medium} />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
+
+// function ResultList(props) {
+//     return (
+//       <ul className="list-group">
+//         {props.results.map(result => (
+//           <li className="list-group-item" key={result.id}>
+//             <img alt={result.title} className="img-fluid" src={result.images.original.url} />
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+
 
 export default EmployeeTable;
