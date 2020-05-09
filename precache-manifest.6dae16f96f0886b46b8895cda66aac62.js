a4e022897bf56d2ea10770ce14ec438f59b11bdd
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-09 14:50:19 -0500

    Updates

---
+++ precache-manifest.6dae16f96f0886b46b8895cda66aac62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a089d7afb1fffa504de4c8519b11d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31663db04f5091ef626",
+    "url": "/static/css/main.6260f27d.chunk.css"
+  },
+  {
+    "revision": "4dc1976584c7a226755c",
+    "url": "/static/js/2.d9b2be1a.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/static/js/2.d9b2be1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31663db04f5091ef626",
+    "url": "/static/js/main.cc8cf693.chunk.js"
+  },
+  {
+    "revision": "99299d3108f175eda0e9",
+    "url": "/static/js/runtime-main.3793437c.js"
+  }
+]);
